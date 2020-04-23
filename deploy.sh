63831951b7e9114e4967e6aa3a94f7cd989c270e
Khoa Do <khoa.do@nkidcorp.com>
2020-04-23 21:43:18 +0700

    added deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t khoad/multi-client:latest -t khoad/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t khoad/multi-server:latest -t khoad/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t khoad/multi-worker:latest -t khoad/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push khoad/multi-client:latest
+docker push khoad/multi-server:latest
+docker push khoad/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push khoad/multi-client:$SHA
+docker push khoad/multi-server:$SHA
+docker push khoad/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=khoad/multi-server:$SHA
+kubectl set image deployments/client-deployment client=khoad/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=khoad/multi-worker:$SHA
--- khd-readme.txt
+++ khd-readme.txt
@@ -1,1 +1,3 @@
 kubectl create secret generic pgpassword --from-literal PGPASSWORD=khd123456
+
+kubectl create secret docker-registry khd-registry-secret --docker-server=https://index.docker.io/v1/ --docker-username={} --docker-password='{}' --docker-email={}
